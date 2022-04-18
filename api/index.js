2338c31f942f7aa594757769f6997d852af3fa10
Aphisit Likitwattanapaisarn <aphisit.heung@gmail.com>
2022-04-18 17:09:42 +0700

    prepare for deploy on vercel

---
+++ api/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    // store: MongoStore.create({
+    //   mongoUrl: process.env.MONGO_DB_URI,
+    // }),
+    resave: false,
+    saveUninitialized: false,
+    // cookie: {
+    //   sameSite: "none",
+    //   httpOnly: true,
+    //   secure: true,
+    // },
+  })
+);
+
+app.get("/api", (req, res, next) => {
+  res.send({ response: "I'm Dead." });
+});
+
+app.use(async (req, res, next) => {
+  await mongoose.connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  return next();
+});
+
+const authRouter = require("./routes/authRouter");
+app.use("/api", authRouter);
+
+const meUserRouter = require("./routes/meUserRouter");
+app.use("/api/user/me", meUserRouter);
+
+module.exports = app;
--- app.js
+++ app.js
@@ -1,57 +1,15 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 4001;
-
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
 const mongoose = require("mongoose");
-const MongoStore = require("connect-mongo");
-const cors = require("cors");
+const app = require("./api/index.js");
+
 require("dotenv").config();
+
+const PORT = process.env.PORT || 4001;
 
 mongoose.connect(process.env.MONGO_DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(cookieParser());
-app.use(
-  session({
-    secret: process.env.SESSION_KEY,
-    // store: MongoStore.create({
-    //   mongoUrl: process.env.MONGO_DB_URI,
-    // }),
-    resave: false,
-    saveUninitialized: false,
-    // cookie: {
-    //   sameSite: "none",
-    //   httpOnly: true,
-    //   secure: true,
-    // },
-  })
-);
-
-app.get("/", (req, res, next) => {
-  res.send({ response: "I'm Dead." });
-});
-
-const authRouter = require("./routes/authRouter");
-app.use(authRouter);
-
-const meUserRouter = require("./routes/meUserRouter");
-app.use("/user/me", meUserRouter);
-
 app.listen(PORT, () => {
-  console.log("listening on http://localhost:" + PORT);
+  console.log(`Server is listening on port ${PORT}`);
 });
